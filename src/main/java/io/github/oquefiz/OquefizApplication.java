9c604d87c214778d1a655bbefe6a8530df54ebe9
Antonio Cleison <a.cleison@live.com>
2026-04-04 22:37:49 -0300

    Initial commit

---
+++ src/main/java/io/github/oquefiz/OquefizApplication.java
@@ -0,0 +1,13 @@
+package io.github.oquefiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OquefizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OquefizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/oquefiz/OquefizApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.oquefiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OquefizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
