e4bee38663d6b9ae39b827c9f558874aff57c3c1
Antonio Cleison <a.cleisonn@gmail.com>
2026-04-10 19:55:10 -0300

    FIX: ALterações de variaveis de ambiente. FEAT: Implementação de Token Service

---
+++ src/main/java/io/github/oquefiz/config/security/TokenService.java
@@ -0,0 +1,54 @@
+package io.github.oquefiz.config.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import io.github.oquefiz.model.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+@Service
+public class TokenService {
+
+    private static final String TOKEN_PREFIX = "Bearer ";
+    private static final String HEADER_STRING = "Authorization";
+    private static final String ISSUER = "o-que-fiz-api";
+
+    @Value("${JWT_SECRET")
+    private String jwtSecret;
+
+    @Value("JWT_EXPIRATION_HOURS:24")
+    private int expirationHours;
+
+    public String generateToken(User user) {
+        Algorithm algorithm = Algorithm.HMAC256(jwtSecret);
+
+        return JWT.create()
+                .withIssuer(ISSUER)
+                .withIssuer(user.getEmail())
+                .withClaim("id", user.getUserId().toString())
+                .withClaim("name", user.getUsername())
+                .withExpiresAt(Instant.now().plus(expirationHours, ChronoUnit.HOURS))
+                .sign(algorithm);
+    }
+
+    public String validateToken(String token) {
+        try {
+
+            Algorithm algorithm = Algorithm.HMAC256(jwtSecret);
+            return JWT.require(algorithm)
+                    .withIssuer(ISSUER)
+                    .build()
+                    .verify(token)
+                    .getSubject();
+
+        } catch (JWTCreationException e) {
+            return null;
+        }
+    }
+
+
+}
