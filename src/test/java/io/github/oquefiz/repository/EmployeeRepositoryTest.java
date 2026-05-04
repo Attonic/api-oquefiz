07fc6643356aad73018398df6dd3e1c24a09e90e
Antonio Cleison <a.cleisonn@gmail.com>
2026-05-04 16:56:39 -0300

    Inicio implementação employee repository teste

--- src/test/java/io/github/oquefiz/repository/EmployeeRepositoryTest.java
+++ src/test/java/io/github/oquefiz/repository/EmployeeRepositoryTest.java
@@ -1,6 +1,6 @@
 package io.github.oquefiz.repository;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,8 +8,20 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest
 @ActiveProfiles("test")
 @Transactional
-@RequiredArgsConstructor
 public class EmployeeRepositoryTest {
+
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+
+
+
+
 
 
 
