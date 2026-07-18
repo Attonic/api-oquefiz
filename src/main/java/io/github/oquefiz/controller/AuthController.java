af8d5d97bb9477105bc3012b85014ba96476e6ce
Antonio Cleison <a.cleisonn@gmail.com>
2026-07-18 19:45:17 -0300

    Implementa documentação por interface para AuthController

--- src/main/java/io/github/oquefiz/controller/AuthController.java
+++ src/main/java/io/github/oquefiz/controller/AuthController.java
@@ -1,5 +1,6 @@
 package io.github.oquefiz.controller;
 
+import io.github.oquefiz.controller.documentation.IAuthControllerDoc;
 import io.github.oquefiz.dto.Request.LoginRequestDto;
 import io.github.oquefiz.dto.Request.RegisterRequestDto;
 import io.github.oquefiz.dto.Response.AuthResponse;
@@ -22,13 +23,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 @RestController
-@Tag(name = "Auth", description = "Contém todas as operações responsáveis pelo login e registro de usuários.")
-public class AuthController {
+
+public class AuthController implements IAuthControllerDoc {
 
     private final AuthService authService;
 
 
-    @Operation(summary = "Faz o login do usuário ao sistema")
+
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(
             @Valid @RequestBody LoginRequestDto requestDto
@@ -37,16 +38,7 @@ public class AuthController {
         return ResponseEntity.ok(authResponse);
     }
 
-    @Operation(summary = "Faz o registro do usuário ao sistema",
-        responses = {
-            @ApiResponse(responseCode = "200", description = "Registro feito com sucesso",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = AuthResponse.class))
-            ),
-            @ApiResponse(responseCode = "409", description = "Conflito já existe este usuário cadastrado.",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ConflictException.class))
-            )
-        }
-    )
+
     @PostMapping("/register")
     public ResponseEntity<AuthResponse> register(
             @Valid @RequestBody RegisterRequestDto requestDto
---
+++ src/main/java/io/github/oquefiz/controller/documentation/IAuthControllerDoc.java
@@ -0,0 +1,46 @@
+package io.github.oquefiz.controller.documentation;
+
+import io.github.oquefiz.dto.Request.LoginRequestDto;
+import io.github.oquefiz.dto.Request.RegisterRequestDto;
+import io.github.oquefiz.dto.Response.AuthResponse;
+import io.github.oquefiz.exception.ConflictException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Tag(name = "Auth", description = "Contém todas as operações responsáveis pelo login e registro de usuários.")
+public interface IAuthControllerDoc {
+
+    @Operation(summary = "Realiza login do usuário.", description = "Faz a autenticação com token JWT junto as credenciais do Usuário.",
+        responses = {
+                @ApiResponse(responseCode = "200", description = "Usuário logado com sucesso.",
+                        content = @Content(mediaType = "application/json", schema = @Schema(implementation = AuthResponse.class))),
+                @ApiResponse(
+                        responseCode = "401",
+                        description = "Credenciais inválidas (E-mail ou senha incorretos)",
+                        content = @Content()),
+                @ApiResponse(
+                responseCode = "400",
+                description = "Dados da requisição inválidos (ex: e-mail em branco)",
+                content = @Content
+                )
+        }
+    )
+    ResponseEntity<AuthResponse> login(LoginRequestDto requestDto);
+
+    @Operation(summary = "Faz o registro do usuário ao sistema",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "Registro feito com sucesso",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = AuthResponse.class))
+            ),
+            @ApiResponse(responseCode = "409", description = "Conflito já existe este usuário cadastrado.",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ConflictException.class))
+            )
+        }
+    )
+    public ResponseEntity<AuthResponse> register(RegisterRequestDto requestDto);
+}
