b311518344c840c12248263afcf1f23ca6aa5545
Antonio Cleison <a.cleisonn@gmail.com>
2026-07-22 18:50:14 -0300

    Implementa documentação swagger para EmployeeController

--- src/main/java/io/github/oquefiz/controller/documentation/IEmployeeControllerDoc.java
+++ src/main/java/io/github/oquefiz/controller/documentation/IEmployeeControllerDoc.java
@@ -2,6 +2,8 @@ package io.github.oquefiz.controller.documentation;
 
 import io.github.oquefiz.dto.Request.EmployeeRequest;
 import io.github.oquefiz.dto.Response.EmployeeResponse;
+import io.github.oquefiz.exception.ConflictException;
+import io.github.oquefiz.exception.NotFoundException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -9,6 +11,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.hibernate.annotations.SoftDelete;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -31,17 +34,48 @@ public interface IEmployeeControllerDoc {
     )
     ResponseEntity<Page<EmployeeResponse>> findAllAtivo(Pageable pageable);
 
-
-    @Operation(summary = "Lista todos os colaboradores")
+    @Operation(summary = "Busca todas os colaboradores.", description = "Carrega todas os colaboradores cadastradas de forma paginada.",
+            responses = {
+                @ApiResponse(responseCode = "200", description = "Colaboradores carregadas com sucesso.",
+                       content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = EmployeeResponse.class)))),
+                @ApiResponse(responseCode = "401", description = "Token ausente ou inválido."),
+                @ApiResponse(responseCode = "403", description = "Usuário autenticado não possui a role ADMIN.")
+            }
+    )
     ResponseEntity<Page<EmployeeResponse>> findAll(Pageable pageable);
 
-    @Operation(summary = "Busca colaborador por Id")
+
+    @Operation(summary = "Busca colaborador por id", description = "Faz a busca de um colaborador pelo seu ID",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "Colaborador encontrada com sucesso",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = EmployeeResponse.class))),
+            @ApiResponse(responseCode = "404", description = "Colaborador não encontrada para o Id.",
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = NotFoundException.class))
+            )
+        }
+    )
     ResponseEntity<EmployeeResponse> findById( UUID id);
 
-    @Operation(summary = "Cadastra novo colaborador.")
+    @Operation(summary = "Cadastra um colaborador.", description = "Faz o cadastro de um novo colaborador.",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "Colaborador criado com sucesso.",
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = EmployeeResponse.class))),
+            @ApiResponse(responseCode = "409", description = "Email usado já existe para um Colaborador.",
+                 content = @Content(mediaType = "application/json", schema = @Schema(implementation = ConflictException.class))
+            )
+        }
+    )
     ResponseEntity<EmployeeResponse> create(@Valid EmployeeRequest request);
 
-    @Operation(summary = "Atualiza um colaborador")
+    @Operation(summary = "Atualiza um colaborador.", description = "Faz o cadastro de um novo colaborador.",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "Colaborador atualizado com sucesso.",
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = EmployeeResponse.class))),
+            @ApiResponse(responseCode = "409", description = "Email usado já existe para um Colaborador.",
+                 content = @Content(mediaType = "application/json", schema = @Schema(implementation = ConflictException.class))
+            )
+        }
+    )
     ResponseEntity<EmployeeResponse> update(UUID id, EmployeeRequest request);
 
 }
